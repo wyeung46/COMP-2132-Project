e56119661288ca346c802bf72784fde8ea6c8881
wyeung46 <wyeung46@my.bcit.ca>
2022-12-05 13:55:15 -0800

    Added jquery just in case Added functionality and jquery connection to index.html Fixed function for closing the rules popup in Project.js

--- js/Project.js
+++ js/Project.js
@@ -1,2 +1,19 @@
 const popup = document.getElementById("popup");
 const closeButton = document.getElementById("closepopup");
+const rulesButton = document.getElementById("rules");
+
+const rulesFadeInterval = 1000;
+
+popup.style.display = "none";
+
+// popupDelayHandler = setTimeout(function() {
+//     popup.style.display = "block";
+// }, popupDelay);
+
+rulesButton.addEventListener("click", function () {
+    popup.style.display = "block";
+});
+
+closeButton.addEventListener("click", function () {
+    popup.style.display = "none";
+});

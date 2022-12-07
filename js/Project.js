858fe2445ed0453505f63ea51a636e240d630bc2
wyeung46 <wyeung46@my.bcit.ca>
2022-12-06 18:16:22 -0800

    Finished functionality for rolling the dice, adding the scores, deciding the winner, and resetting the game
    Also created popups telling the user
    if they won, lost, or tied

--- js/Project.js
+++ js/Project.js
@@ -1,22 +1,40 @@
-const popup = document.getElementById("popup");
+const rules = document.getElementById("rules");
 const closeButton = document.getElementById("closepopup");
-const rulesButton = document.getElementById("rules");
-const $popup = $("#popup");
+const rulesButton = document.getElementById("rulesButton");
+const $rules = $("#rules");
 const $closeButton = $("#closepopup");
-const $rulesButton = $("#rules");
+const $rulesButton = $("#rulesButton");
+const $playerdie1 = $("#playerdie1");
+const $playerdie2 = $("#playerdie2");
+const $opponentdie1 = $("#opponentdie1");
+const $opponentdie2 = $("#opponentdie2");
+const $playerRoundScore = $(".playerRoundScore");
+const $playerTotalScore = $(".playerTotalScore");
+const $opponentRoundScore = $(".opponentRoundScore");
+const $opponentTotalScore = $(".opponentTotalScore");
+const $rollDice = $("#rollDice");
+const $resetGame = $(".resetGame");
+const $win = $("#win");
+const $lose = $("#lose");
+const $tie = $("#tie");
+const $result = $(".result");
+const popupFadeInterval = 166;
 
-const rulesFadeInterval = 166;
-let userScore = 0;
+let userScore = 0;  
 let opponentScore = 0;
+let userRoundScore = 0;
+let opponentRoundScore = 0;
+let roundCounter = 0;
+let scoreCounter = 0;
 
-popup.style.display = "none";
+rules.style.display = "none";
 
 $rulesButton.click(function () {
-    $popup.fadeIn(rulesFadeInterval);
+    $rules.fadeIn(popupFadeInterval);
 });  
 
 $closeButton.click(function () {
-    $popup.fadeOut(rulesFadeInterval);
+    $rules.fadeOut(popupFadeInterval);
 });
 
 class Player {
@@ -26,5 +44,75 @@ class Player {
     }
 }
 
+class Dice {
+    constructor() {
+        this.roll1;
+        this.roll2;
+    }
+}
+
 let user = new Player("user");
 let opponent = new Player("opponent");
+let dice = new Dice();
+
+function rollDice() {
+    dice.roll1 = Math.floor(Math.random() * 6) + 1;
+    dice.roll2 = Math.floor(Math.random() * 6) + 1;
+    if((dice.roll1 == 1) || (dice.roll2 == 1)){
+        scoreCounter = 0;
+    }
+    else if (dice.roll1 == dice.roll2) {
+        scoreCounter = (dice.roll1 + dice.roll2) * 2;
+    }
+    else {
+        scoreCounter = dice.roll1 + dice.roll2;
+    }
+}
+
+$rollDice.click(function () {
+    rollDice();
+    $playerdie1.attr({
+        "src": `../images/whitedie${dice.roll1}.svg`
+    });
+    $playerdie2.attr({
+        "src": `../images/whitedie${dice.roll2}.svg`
+    });
+    userScore += scoreCounter;
+    $playerRoundScore.text(`${scoreCounter}`);
+    $playerTotalScore.text(`${userScore}`);
+    rollDice();
+    $opponentdie1.attr({
+        "src": `../images/blackdie${dice.roll1}.svg`
+    });
+    $opponentdie2.attr({
+        "src": `../images/blackdie${dice.roll2}.svg`
+    });
+    opponentScore += scoreCounter;
+    $opponentRoundScore.text(`${scoreCounter}`);
+    $opponentTotalScore.text(`${opponentScore}`);
+    roundCounter++;
+    if (roundCounter == 3){
+        if(userScore > opponentScore) {
+            $win.fadeIn(popupFadeInterval);
+        }
+        if(userScore < opponentScore) {
+            $lose.fadeIn(popupFadeInterval);
+        }
+        if(userScore == opponentScore) {
+            $tie.fadeIn(popupFadeInterval);
+        }
+        roundCounter = 0;
+    }
+
+});
+
+$resetGame.click(function() {
+    $result.css('display', 'none');
+    roundCounter, userScore, opponentScore, userRoundScore, opponentRoundScore = 0;
+    $playerRoundScore.text(`0`);
+    $playerTotalScore.text(`0`);
+    $opponentRoundScore.text(`0`);
+    $opponentTotalScore.text(`0`);
+});
+
+    
